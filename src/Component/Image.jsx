e5a6fc3c153d8026ffb7370dd533364e0f5ad7c3
amypo-reactapp <reactapp@amypo.in>
2024-02-20 09:28:16 +0000

    User

--- src/Component/Image.jsx
+++ src/Component/Image.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
 
-// Sample React component for the logo
-const ReactLogo = () => (
-  <img
-    src=""
-    alt="React Logo"
-    style={{ maxWidth: '100px', maxHeight: '100px' }}
-  />
-);
 
 // Main React Skill Session Component
 const ReactSkillSession = () => (
   <div>
     <h6>React Skill Session</h6>
-    <p>
-      React is a JavaScript library for building user interfaces. It allows
-      developers to create reusable UI components and efficiently update the
-      user interface in response to data changes. React follows a
-      component-based architecture, making it easier to manage and scale
-      complex UIs.
-    </p>
-    <ReactLogo />
+   
   </div>
 );
 
